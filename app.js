55f160a76c29ead8043512e725a9e37ecbd34287
Tridev Sharma <sendmailtotridev@gmail.com>
2024-07-29 10:21:51 +0530

    xss-clean, compression and cors added

--- app.js
+++ app.js
@@ -14,13 +14,19 @@ const express = require("express");
 const helmet = require("helmet");
 const expressRateLimit = require("express-rate-limit");
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
+const compression = require('compression');
+const xssClean = require('xss-clean');
 const applicationRouteHandler = require("@routes"); //main route handler
+
+//service load
 const applicationErrorHandler = require("@services/application-error-handler.service");
 
 //get all the configs
 const helmetOptions = require("@config/helmet.config");
 const rateLimitOptions = require("@config/express-rate-limit.config");
 const httpLogger = require("@config/http-logger.config");
+const corsOptions = require("@config/cors.config");
 
 //get all the middlewares
 const responseBodyInterceptor = require("@middlewares/response-interceptor.middleware");
@@ -32,11 +38,22 @@ const app = express();
 //security middlewares
 app.use(helmet(helmetOptions));
 
+//logger
+app.use(httpLogger());
+
+//cors setup
+app.use(cors(corsOptions));
+
+// Compression middleware
+app.use(compression());
+
 //express middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(cookieParser());
+
+//xss middleware
+app.use(xssClean());
 
 // Apply CSRF protection middleware
 app.use(csrfProtection);
@@ -46,9 +63,6 @@ app.use(expressRateLimit(rateLimitOptions));
 
 // response body interceptor middleware
 app.use(responseBodyInterceptor);
-
-//logger
-app.use(httpLogger());
 
 //handle all the requests
 app.use(applicationRouteHandler);
---
+++ src/config/cors.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  origin: "localhost", // Replace with your allowed origin
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
